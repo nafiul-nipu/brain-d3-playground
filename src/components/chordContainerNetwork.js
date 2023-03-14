ae6981a8aadb85840b261e915b68203a72ca44ab
Nafiul Nipu <nafiulnipu.2013@gmail.com>
2023-03-14 15:39:41 -0500

    code removed

--- src/components/chordContainerNetwork.js
+++ src/components/chordContainerNetwork.js
@@ -11,11 +11,11 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
 
     useEffect(() => {
         // console.log(networkdata)
-        if (network || networkEvent || eventKey) {
+        // if (network || networkEvent || eventKey) {
             setShowParagraph(true);
-        }
+        // }
 
-    }, [eventKey, network, networkEvent]);
+    }, []);
 
     let networkdata;
 
@@ -30,18 +30,6 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
 
 
     }
-
-    // if (!network) {
-    //     return (<div>data loading</div>)
-    // }
-
-    // console.log(network)
-    // console.log(networkEvent)
-
-    // console.log(data)
-
-    // let networkdata = data
-    // let networkdata = network
 
     const rois = [100, 101, 201, 300, 301, 400, 401, 501]
 
@@ -66,6 +54,11 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
         .sortSubgroups(d3.descending)
         .sortChords(d3.descending)
 
+    const donArc = d3.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius)
+        .padAngle(0.08)
+
     const textPadding = 1.2
 
     // console.log(chords)
@@ -82,32 +75,23 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
             <g>
                 {
                     networkdata.map((nd, i) => {
-                        // console.log(nd)
+            
                         if (nd.roi === 'rest') {
                             const uniqueNames = [...new Set(nd.netWithCount.map(item => item.count))];
                             uniqueNames.sort((a, b) => a - b);
-                            // console.log(uniqueNames)
+                      
                             const strokeRange = Array.from({ length: uniqueNames.length }, (_, i) => 1 + i * 0.25);
-                            // console.log(strokeRange)
+                         
                             const strokeWidthScale = d3.scaleOrdinal()
                                 .domain(uniqueNames)
                                 .range(strokeRange)
 
-                            // console.log(d3.select(`#roi_100`).node().getBBox());
                             return (
                                 showParagraph && nd.netWithCount.map((each, i) => {
-                                    // console.log(each)
-                                    // console.log(roiElectrode[each.source])
-                                    // console.log(d3.select(`#el_${each.source}`).node().getBoundingClientRect())
-                                    // console.log(d3.select(`#el_${each.target}`).node().getBoundingClientRect())
+
 
                                     let sourceroi = rois.indexOf(roiElectrode[each.source])
                                     let targetroi = rois.indexOf(roiElectrode[each.target])
-
-                                    let sourceGroupPosition = [x[sourceroi], y[sourceroi]];
-                                    let targetGroupPosition = [x[targetroi], y[targetroi]];
-
-                                    // console.log(sourceGroupPosition, targetGroupPosition)
 
                                     let sourceBox = d3.select(`#el_${each.source}`).node().getBoundingClientRect()
                                     let targetBox = d3.select(`#el_${each.target}`).node().getBoundingClientRect()
@@ -122,20 +106,6 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
                                     // translate(-280, 0)  === 0
                                     return (
                                         <g className='aGroup' transform="translate(-280, 0)">
-                                            {/* <defs>
-                                            <marker
-                                                id="arrow"
-                                                markerWidth="10"
-                                                markerHeight="10"
-                                                refX="0"
-                                                refY="3"
-                                                orient="auto"
-                                                markerUnits="strokeWidth"
-
-                                            >
-                                                <path d="M0,0 L0,6 L9,3 z" fill="black" opacity={0.5} />
-                                            </marker>
-                                        </defs> */}
                                             <line
                                                 x1={source[0]}
                                                 y1={source[1]}
@@ -143,26 +113,19 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
                                                 y2={target[1]}
                                                 stroke="black" strokeWidth={strokeWidthScale(each.count)} markerEnd="url(#arrow)" strokeOpacity={0.6}
                                             ></line><title>{`${+each.source} -> ${+each.target} = ${+each.count}`}</title>
-                                            {/* <path
-                                            stroke='grey'
-                                            fill='none'
-                                            d={d3.linkHorizontal()
-                                                .source(d => d.source)
-                                                .target(d => d.target)
-                                                (link)
-                                            }
-                                        /> */}
                                         </g>
                                     )
 
                                 })
                             )
                         }
-                        // else
+                        else
                         if (nd.roi !== 'rest' && nd.network.length !== 0) {
                             // console.log(nd)
                             let data = Object.assign(nd.matrix, { names: nd["electrodes"], colors: colorList })
                             const chords = chord(data)
+                            console.log(data)
+                            console.log(chords)
                             const names = data.names === undefined ? d3.range(data.length) : data.names
                             const colors = data.colors === undefined ? d3.quantize(d3.interpolateRainbow, names.length) : data.colors
                             const color = d3.scaleOrdinal(names, colors)
@@ -170,6 +133,7 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
                                 // <svg width={width} height={height}>
                                 <g transform={`translate(${x[i]}, ${y[i]})`} id={`roi_${nd.roi}`}>
                                     {chords.groups.map((each) => {
+                                        // console.log(chordArc())
                                         // console.log(each)
                                         let textTransform = chordArc.centroid(each);
                                         return (
@@ -232,19 +196,12 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
                             var pie = d3.pie()
                                 .value(function (d) { return d[1]; })
                             var data_ready = pie(Object.entries(obj))
-                            // console.log(Object.entries(obj))
-                            // console.log(data_ready)
-
-                            const donArc = d3.arc()
-                                .innerRadius(innerRadius)
-                                .outerRadius(outerRadius)
-                                .padAngle(0.08)
-
+                            
                             return (
                                 // <svg width={width} height={height}>
                                 <g transform={`translate(${x[i]}, ${y[i]})`} id={`roi_${nd.roi}`}>
                                     {data_ready.map((each, i) => {
-                                        // console.log(each)
+                                        console.log(each)
                                         let textTransform = donArc.centroid(each);
                                         return (
                                             <g key={i}>
@@ -275,70 +232,6 @@ export const ChordContainerNetwork = ({ network, networkEvent, eventKey }) => {
                                 // </svg>
                             )
                         }
-                        // else {
-                        //     const uniqueNames = [...new Set(nd.roiWithCount.map(item => item.count))];
-                        //     uniqueNames.sort((a, b) => a - b);
-                        //     // console.log(uniqueNames)
-                        //     const strokeRange = Array.from({ length: uniqueNames.length }, (_, i) => 1 + i * 0.25);
-                        //     // console.log(strokeRange)
-                        //     const strokeWidthScale = d3.scaleOrdinal()
-                        //         .domain(uniqueNames)
-                        //         .range(strokeRange)
-
-                        //     // console.log(d3.select(`#roi_100`).node().getBBox());
-                        //     return (
-                        //         showParagraph && nd.netWithCount.slice(0, 1).map((each, i) => {
-                        //             console.log(each)
-                        //             console.log(nd.roiWithCount[i])
-                        //             console.log(d3.select(`#el_${each.source}`).node().getBoundingClientRect())
-                        //             console.log(d3.select(`#el_${each.target}`).node().getBoundingClientRect())
-                        //             let sourceBox = d3.select(`#el_${each.source}`).node().getBoundingClientRect()
-                        //             let targetBox = d3.select(`#el_${each.target}`).node().getBoundingClientRect()
-                        //             // { x: rect.left + rect.width / 2, y: rect.top + rect.height / 2 }
-                        //             let source = [sourceBox.left - 15 + sourceBox.width / 2, sourceBox.top + sourceBox.height / 2]
-                        //             let target = [targetBox.left + 15 + targetBox.width / 2, targetBox.top + targetBox.height / 2]
-
-                        //             // let source = [sourceBox.x, sourceBox.y]
-                        //             // let target = [targetBox.x, targetBox.y]
-
-                        //             return (
-                        //                 <g className='aGroup' transform="translate(-280, 0)">
-                        //                     <defs>
-                        //                         <marker
-                        //                             id="arrow"
-                        //                             markerWidth="10"
-                        //                             markerHeight="10"
-                        //                             refX="0"
-                        //                             refY="3"
-                        //                             orient="auto"
-                        //                             markerUnits="strokeWidth"
-
-                        //                         >
-                        //                             <path d="M0,0 L0,6 L9,3 z" fill="black" opacity={0.5} />
-                        //                         </marker>
-                        //                     </defs>
-                        //                     <line
-                        //                         x1={source[0]}
-                        //                         y1={source[1]}
-                        //                         x2={target[0]}
-                        //                         y2={target[1]}
-                        //                         stroke="black" strokeWidth={strokeWidthScale(each.count)} markerEnd="url(#arrow)" strokeOpacity={0.4}
-                        //                     ></line><title>{`${+each.source} -> ${+each.target} = ${+each.count}`}</title>
-                        //                     {/* <path
-                        //                         stroke='grey'
-                        //                         fill='none'
-                        //                         d={d3.linkHorizontal()
-                        //                             .source(d => d.source)
-                        //                             .target(d => d.target)
-                        //                             (link)
-                        //                         }
-                        //                     /> */}
-                        //                 </g>
-                        //             )
-
-                        //         })
-                        //     )
-                        // }
                     })
                 }
             </g>
